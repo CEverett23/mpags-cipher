3a3d272c0e9ce11f263781fd5fd8b3cbf12a9ce3
Chris Everett <C.Everett@warwick.ac.uk>
2015-10-23 14:51:47 +0100

    Exercise 2 - 23/10  Please enter the commit message for your changes. Lines starting

---
+++ mpags-cipher.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+  //This is my message to youuuuuu
+
+   std::cout << "Hey babe\n";
+
+ int d {1};
+ d = 232;
+ 
+ std::cout << d << std::endl;
+
+ int a {1};
+ a=25;
+ double b {0.1};
+ b=1.5;
+ double c;
+ double e;
+ 
+ c = b/a;
+
+ e = a*b;
+ 
+ std::cout << c << std::endl;
+
+ std::cout << e << std::endl;
+
+ int f;
+ f= c*e;
+
+ std::cout << f << std::endl;
+
+ double g {2.34};
+
+ double h = g/b;
+
+ std::cout << h << std::endl;
+}
