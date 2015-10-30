100aeb7ab5bd2aa7ea1b0eff57d18baf39dc3b13
Chris Everett <C.Everett@warwick.ac.uk>
2015-10-30 13:22:32 +0000

    Merging files

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,6 +1,63 @@
 #include<iostream>
 #include<string>
 
+
+std::string transformChar(char input )
+{
+  std::string output{""};
+  
+  switch(input)
+    {
+      //Taking numerical characters and replacing with words
+    case'0':
+      output += "ZERO";
+      break;
+
+    case '1':
+      output += "ONE";
+      break;
+      
+    case '2' :
+      output += "TWO";
+      break;
+      
+    case '3':
+      output += "THREE";
+      break;
+      
+    case'4':
+      output += "FOUR";
+      break;
+      
+    case '5':
+      output += "FIVE";
+      break;
+      
+    case '6' :
+      output += "SIX";
+      break;
+      
+    case '7':
+      output += "SEVEN";
+      break;
+      
+    case'8':
+      output += "EIGHT";
+      break;
+      
+    case '9':
+      output += "NINE";
+      break;
+      
+      //Numbers outputted as words, with spaces between.
+      
+    }
+  return output;
+}
+
+
+
+
 int main(int argc, char* argv[])
 {
 
@@ -8,60 +65,13 @@ int main(int argc, char* argv[])
   
   char in_char{'x'};
   while(std::cin >> in_char)
-
     //Input of original message, outputs on Enter then Ctrl+D
     {
-    switch(in_char)
-      {
-	//Taking numerical characters and replacing with words
-      case'0':
-	std::cout<<("ZERO ");
-	break;
-
-      case '1':
-	std::cout<<("ONE ");
-	break;
-
-      case '2' :
-	std::cout<<("TWO ");
-	break;
-
-      case '3':
-	std::cout<<("THREE ");
-	break;
-
-      case'4':
-	std::cout<<("FOUR ");
-	break;
-
-      case '5':
-	std::cout<<("FIVE ");
-	break;
-
-      case '6' :
-	std::cout<<("SIX ");
-	break;
-
-      case '7':
-	std::cout<<("SEVEN ");
-	break;
-
-	 case'8':
-	std::cout<<("EIGHT ");
-	break;
-
-      case '9':
-	std::cout<<("NINE ");
-	break;
-
-	//Numbers outputted as words, with spaces between.
-     
-      }
+      std::cout << transformChar(in_char);
     }
-
-
-
 }
+
+
 
 	  
 
